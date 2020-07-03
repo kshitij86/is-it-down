87ae37cf173de3e80051714c8f6ebc09bb25aa57
Kshitij Kotasthane <kshitij.kotasthane@gmail.com>
2020-07-04 01:17:02 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    lyrics = None
+    artist = None
+
+    if request.method == 'POST':
+        lyrics = request.form['lyrics']
+        return render_template('index.html', lyrics=lyrics, artist=artist)
+
+    else:
+        return render_template('index.html', lyrics=lyrics, artist=artist)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
